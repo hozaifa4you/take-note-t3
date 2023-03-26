c10567b199203a9f6b1d4dd9a0539fb3e03842ed
yousuf4you <yousuf360.ya@gmail.com>
2023-03-26 12:22:27 +0600

    get all topics and fetch all topics

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { createTRPCRouter } from "@/server/api/trpc";
-import { exampleRouter } from "@/server/api/routers/example";
+import { createTRPCRouter } from '@/server/api/trpc';
+import { exampleRouter } from '@/server/api/routers/example';
+import { topicRouter } from '@/server/api/routers/topic';
+import { noteRouter } from './routers/notes';
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { exampleRouter } from "@/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+   example: exampleRouter,
+   topic: topicRouter,
+   note: noteRouter,
 });
 
 // export type definition of API
